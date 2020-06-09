0254218132bf54a881b3bfaa4d10e215fe372964
ramikh96 <ramikh.developer@gmail.com>
2020-06-10 01:47:50 +0200

    add cookie-session and passport middlewares to app

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const app = express();
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const passport = require('passport');
+const cookieSession = require('cookie-session');
 require('./db/models/user');
 require('./services/passport'); // because we're not requiring anything from it
 var indexRouter = require('./routes/index');
 
 // Binding routes
+app.use(cookieSession({
+    maxAge: 30*24*60*60*1000, // last for 30 days
+    keys: [keys.cookieKey]
+}));
 app.use(passport.initialize());
+app.use(passport.session()); // to use cookies
 app.use('/v1/easy-survey',indexRouter(app));
 // Connect to MongoDB database
 mongoose.connect(keys.mongoURI, {
