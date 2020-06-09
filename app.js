b21e883138cda48049a11c0646f2214f6f7137b8
ramikh96 <ramikh.developer@gmail.com>
2020-06-09 23:15:50 +0200

    install mongoose and connect to mongodb atlas

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport'); // because we're not requiring anything from it
 var indexRouter = require('./routes/index');
+
+// Binding routes
 app.use('/v1/easy-survey',indexRouter(app));
 
+// Connect to MongoDB database
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Listen to the enviroment port (Heroku) or the development port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
