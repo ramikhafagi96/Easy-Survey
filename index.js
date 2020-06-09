b0c057659cabc1bd4200b41adfe0a0a4028de5a3
ramikh96 <ramikh.developer@gmail.com>
2020-06-09 17:48:18 +0200

    setup google startegy for passport and authentication route

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const passport = require('passport');
+const keys = require('./config/keys');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
-
 const PORT = process.env.PORT || 5000;
+const CALLBACKURL = '/auth/google/callback';
 
-app.get('/', (req,res) => {
+// Passport Configuration
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: CALLBACKURL
+        },
+        accessToken => {
+            console.log(accessToken);
+        }
+    )
+);
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ["profile", "email"]
+}));
+
+app.get('/', (req, res) => {
     res.send({ message: "Up and Running!" });
 });
 
