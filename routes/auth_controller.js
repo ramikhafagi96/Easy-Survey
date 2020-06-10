f7799dabdce07904020cd900dc3e5807b72514cd
ramikh96 <ramikh.developer@gmail.com>
2020-06-10 03:54:32 +0200

    redirect to homepage

--- routes/auth_controller.js
+++ routes/auth_controller.js
@@ -7,9 +7,9 @@ router.get('/google', passport.authenticate('google', {
     scope: ["profile", "email"] // info we need from google about the user
 }));
 
-router.get('/google/callback', passport.authenticate('google', (req,res) => {
+router.get('/google/callback', passport.authenticate('google'), (req,res) => {
     res.redirect('/');
-}));
+});
 
 router.get('/logout', (req,res) => {
     req.logOut(); // it takes the cookie that contais the user id and kill the cookie
