0940a6137746e7d2ab7292292a1208b842d31c46
ramikh96 <ramikh.developer@gmail.com>
2020-06-11 20:27:26 +0200

    add requireLogin middleware to check if user logged in

--- routes/user_controller.js
+++ routes/user_controller.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { chargePayment } = require('../services/stripe');
 const userServices = require('../db/models_services/user_services');
-
+const requireLogin = require('../middlewares/requireLogin');
 
 router.get('/', (req, res) => {
     res.send(req.user);
 });
 
-router.post('/payment', async (req, res) => {
+router.post('/payment', requireLogin, async (req, res) => {
     const charge = await chargePayment(req.body.id);
     const user = await userServices.updateUserCredits(req.user.googleId);
     res.send(user);
