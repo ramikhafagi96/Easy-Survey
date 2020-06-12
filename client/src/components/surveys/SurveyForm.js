cf98735ed743353586197f077d58956250243bf9
ramikh96 <ramikh.developer@gmail.com>
2020-06-12 20:20:54 +0200

    render SurveyField component in the SurveyForm

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form'; // allows our component to communicate with redux store
+import { Link } from 'react-router-dom';
+import SurveyField from './SurveyField';
 
+const FIELDS = [
+    {label:'Survey Title', name:'title'},
+    {label:'Subject Line', name:'subject'},
+    {label:'Email Body', name:'email'},
+    {label:'Recipient List', name:'emails'}
+];
 class SurveyForm extends Component {
+    renderFields() {
+        return FIELDS.map(({label, name}) => {
+            return <Field key={name} component={SurveyField} type="text" label={label} name={name}/>
+        })
+    }
     render() {
-        return(
+        return (
             <div>
-                Survey Form
+                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                    {this.renderFields()}
+                    <Link to="/surveys" className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button type="submit" className="teal btn-flat right white-text">
+                        Next
+                        <i className="material-icons right">done</i>
+                    </button>
+                </form>
             </div>
         );
     }
 }
 
-export default SurveyForm;
+
+
+export default reduxForm({
+    form: 'surveyForm'
+})(SurveyForm);
