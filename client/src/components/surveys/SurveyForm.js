43646e2c8041428446b0f942b7da3c7c1fe94920
ramikh96 <ramikh.developer@gmail.com>
2020-06-12 21:01:15 +0200

    validate fields and validate emails

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form'; // allows our component to communicate with redux store
 import { Link } from 'react-router-dom';
+import validateEmails from '../../utils/validateEmails';
 import SurveyField from './SurveyField';
 
 const FIELDS = [
-    {label:'Survey Title', name:'title'},
-    {label:'Subject Line', name:'subject'},
-    {label:'Email Body', name:'email'},
-    {label:'Recipient List', name:'emails'}
+    { label: 'Campaign Title', name: 'title' },
+    { label: 'Subject Line', name: 'subject' },
+    { label: 'Email Body', name: 'body' },
+    { label: 'Recipient List', name: 'recipients' }
 ];
 class SurveyForm extends Component {
     renderFields() {
-        return FIELDS.map(({label, name}) => {
-            return <Field key={name} component={SurveyField} type="text" label={label} name={name}/>
+        return FIELDS.map(({ label, name }) => {
+            return <Field key={name} component={SurveyField} type="text" label={label} name={name} />
         })
     }
     render() {
@@ -34,7 +35,17 @@ class SurveyForm extends Component {
 }
 
 
+function validate(values) {
+    const errors = {}; // errros if occured
+    errors.recipients = validateEmails(values.recipients || '');
+    FIELDS.forEach(({ name }) => {
+        if (!values[name])
+            errors[name] = 'You must provide a value';
+    });
+    return errors;
+}
 
 export default reduxForm({
+    validate: validate,
     form: 'surveyForm'
 })(SurveyForm);
