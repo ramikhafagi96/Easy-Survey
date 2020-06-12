5836c2b70c7ef5d89c7b70fb2355155318cd0266
ramikh96 <ramikh.developer@gmail.com>
2020-06-12 21:33:03 +0200

    create SurveyFormReview component and show it afrer form submit

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const SurveyForm = () => {
+    return(
+        <div>
+            <h5>Please Confirm Your Entries</h5>
+        </div>
+    );
+}
+
+export default SurveyForm;
