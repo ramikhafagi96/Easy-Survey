4755d5443c9409d3f72980963c6341ccbf1654dd
ramikh96 <ramikh.developer@gmail.com>
2020-06-12 21:44:11 +0200

    add back button in the SurveyFormReview component

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
-const SurveyForm = () => {
+const SurveyForm = ({ onCancel }) => {
     return(
         <div>
             <h5>Please Confirm Your Entries</h5>
+            <button
+            className="yellow darken-3 btn-flat"
+            onClick={onCancel}
+            >
+                Back
+            </button>
         </div>
     );
 }
