f9a3780906b4a736bf463af5c137a84bf809474d
ramikh96 <ramikh.developer@gmail.com>
2020-06-12 02:13:36 +0200

    save created survey in a const variable

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
-const survey_controller = require('./survey_controller');
 var router = express.Router();
 // Require Routes
-auth_controller = require('./auth_controller');
-user_controller = require('./user_controller');
-survey_controller = require('./survey_controller');
+const auth_controller = require('./auth_controller');
+const user_controller = require('./user_controller');
+const survey_controller = require('./survey_controller');
 module.exports = () => {
     auth_controller(router);
     user_controller(router);
--- routes/survey_controller.js
+++ routes/survey_controller.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const surveyServices = require('../db/models_services/survey_services');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
-router.post('/survey', requireLogin, requireCredits, (req, res) => {
-    surveyServices.createSurvey(req.body,req.user.id);
+router.post('/survey', requireLogin, requireCredits, async (req, res) => {
+    const survey = surveyServices.createSurvey(req.body,req.user.id);
 });
 
 module.exports = function (indexRouter) {
