d518a2e0b09ad616392974c0285c4170c41990aa
ramikh96 <ramikh.developer@gmail.com>
2020-06-13 18:32:06 +0200

    show dashboard button if logged in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -12,11 +12,18 @@ class Header extends Component {
                 return <li><a href="/api/auth/google">Login With Google</a></li>;
             default:
                 return [
-                <li key="1"><Payments /></li>,
-                <li key="2" style={{ margin: '0 10px'}}>
-                    Credits: {this.props.auth.credits}
-                </li>,
-                <li key="3"><a href="/api/auth/logout">Logout</a></li>
+                    <li key="1"><Payments /></li>,
+                    <li><Link
+                        to={'/surveys'}
+                        className="right"
+                    >
+                        Dashboard
+                    </Link>
+                    </li>,
+                    <li key="2" style={{ margin: '0 10px' }}>
+                        Credits: {this.props.auth.credits}
+                    </li>,
+                    <li key="3"><a href="/api/auth/logout">Logout</a></li>
                 ]
         }
     }
@@ -25,10 +32,10 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <Link 
-                        to={this.props.auth ? '/surveys' : '/'} 
+                    <Link
+                        to={this.props.auth ? '/surveys' : '/'}
                         className="left brand-logo"
-                        style={{ marginLeft: '10px'}}
+                        style={{ marginLeft: '10px' }}
                     >
                         Easy Survey
                     </Link>
